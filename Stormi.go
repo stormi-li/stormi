725b2b8df043584b9aede286752046a242f7da2a
训练小猪天上飞 <2785782829@qq.com>
2024-05-10 11:27:56 +0800

    3.0.6

--- Stormi.go
+++ Stormi.go
@@ -2,63 +2,9 @@ package stormi
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/stormi-li/stormi/dirandfileopt"
-	"github.com/stormi-li/stormi/formatprint"
-	"github.com/stormi-li/stormi/proxy"
 )
 
-func Init() {
-	dirandfileopt.CreateDirInCurrentDir("app-redis-cluster")
-	dirandfileopt.CreateDirInCurrentDir("app-nsqd-cluster")
-	dirandfileopt.CreateDirInCurrentDir("app-redis-node")
-	dirandfileopt.CreateDirInCurrentDir("mysql")
-	dirandfileopt.CreateDirInCurrentDir("nsqd")
-	dirandfileopt.CreateDirInCurrentDir("protocode")
-	dirandfileopt.CreateDirInCurrentDir("server-rpc")
-	dirandfileopt.CreateDirInCurrentDir("server-web")
-	dirandfileopt.CreateDirInCurrentDir("server-proxy")
-	currentDir, _ := os.Getwd()
-	if dirandfileopt.ExistFile(currentDir + "/app.config") {
-		formatprint.FormatPrint(formatprint.Magenta, "文件已存在: "+currentDir+"/app.config")
-	} else {
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.config", "")
-		formatprint.FormatPrint(formatprint.Blue, "文件创建成功: "+currentDir+"/app.config")
-	}
-	if dirandfileopt.ExistFile(currentDir + "/app.yaml") {
-		formatprint.FormatPrint(formatprint.Magenta, "文件已存在: "+currentDir+"/app.yaml")
-	} else {
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "stormi:\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "  ip: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "  mysql:\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    username: root\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    password: 123456\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    host: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    port: 3306\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    dbname: stormi\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    timeout: 5s\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    skipdefaulttransaction: true\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "  rpcserver:\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    name: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    weight: 1\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    port: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "  webserver:\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    name: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    port: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "  redis:\n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    node: \n")
-		dirandfileopt.AppendToConfigFile(currentDir+"/app.yaml", "    nodes: \n")
-		formatprint.FormatPrint(formatprint.Blue, "文件创建成功: "+currentDir+"/app.yaml")
-	}
-
-	formatprint.FormatPrint(formatprint.Yellow, "stormi初始化完成, 使用前请配置好ip地址")
-}
-
-func Exec(cmd string) {
-	proxy.ExecCommand(cmd)
-}
-
-func Version() {
-	fmt.Println("v1.1.1")
+func GitHubUrl() {
+	fmt.Println(green+"源代码地址: https://github.com/stormi-li/stormi", reset)
+	fmt.Println(green+"资源地址: https://github.com/stormi-li/stormi-bin-resource", reset)
 }
---
+++ Utils.go
@@ -0,0 +1,29 @@
+package stormi
+
+import (
+	"time"
+)
+
+type ticker struct {
+	time time.Time
+}
+
+type utils struct{}
+
+var Utils utils
+
+func (utils) NewTimer() ticker {
+	t := ticker{
+		time: time.Now(),
+	}
+	return t
+}
+
+func (tt ticker) Stamp() time.Duration {
+	return time.Since(tt.time)
+}
+
+func (tt *ticker) StampAndReset() time.Duration {
+	tt.time = time.Now()
+	return time.Since(tt.time)
+}
