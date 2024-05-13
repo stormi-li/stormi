79ea8143ea05fff045366e31596108a1e8504c26
训练小猪天上飞 <2785782829@qq.com>
2024-05-13 20:14:01 +0800

    v1.1.6

--- test2/Test.go
+++ test2/Test.go
@@ -1,27 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/stormi-li/stormi"
-	UserServer "github.com/stormi-li/stormi/coprotocol/UserServer"
-)
+import "github.com/stormi-li/stormi"
 
 func main() {
-	cop := stormi.NewCooperationProxy(stormi.NewConfigProxy(stormi.NewRedisProxy("127.0.0.1:2131")), "UserServer")
-	caller := cop.NewCaller()
-	caller.SetTimeout(10 * time.Second)
-	caller.SetConcurrency(100)
-	for i := 0; i < 50; i++ {
-		go func() {
-			for {
-				dto := UserServer.UserServerDto{}
-				caller.Call(UserServer.Insert, UserServer.UserServerDto{Id: 1, UserName: "stormi"}, &dto)
-				fmt.Println(dto)
-				time.Sleep(1 * time.Second)
-			}
-		}()
-	}
-	select {}
+	rp := stormi.NewRedisProxy("127.0.0.1:213")
+	rp.RedisSingleNodeInfo()
 }
