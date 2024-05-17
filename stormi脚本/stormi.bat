e51c51fd491c335871df140d97bf342b6bab9f7a
lili <2785782829@qq.com>
2024-05-17 14:41:48 +0800

    v1.1.7

--- stormi脚本/stormi.bat
+++ stormi脚本/stormi.bat
@@ -1,5 +1,5 @@
 @echo off
-
+setlocal enabledelayedexpansion
 if "%1"=="upload" (
     git add .
     git commit -m v1.1.7
@@ -8,3 +8,11 @@ if "%1"=="upload" (
 )
 
 
+if "%1"=="nsqdkill" (
+    for /f "tokens=2" %%a in ('tasklist ^| findstr nsqd') do (
+        set /a "PID=%%a"
+        echo Terminating nsqd process with PID !PID!...
+        taskkill /PID !PID! /F > nul
+    )
+    exit
+)
