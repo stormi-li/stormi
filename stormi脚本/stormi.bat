acd3d4917bddb0f2725f79138eadb09200a429f0
lili <2785782829@qq.com>
2024-05-17 14:33:54 +0800

    v1.1.7

--- stormi脚本/stormi.bat
+++ stormi脚本/stormi.bat
@@ -1,6 +1,10 @@
 @echo off
+
+if "%1"=="upload" (
     git add .
     git commit -m v1.1.7
     git push origin master
     exit
+)
+
 
--- test2/Test.go
+++ test2/Test.go
@@ -1,19 +1,9 @@
 package main
 
 import (
-	"bufio"
-	"os"
-	"strings"
-
 	"github.com/stormi-li/stormi"
 )
 
 func main() {
-	rp := stormi.NewRedisProxy("127.0.0.1:2131")
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		text, _ := reader.ReadString('\n')
-		text = strings.TrimSpace(text)
-		rp.Notify("stormi-chat", text)
-	}
+	stormi.NodeBuilder.Install()
 }
