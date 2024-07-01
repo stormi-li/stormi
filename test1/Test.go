b02985bbad9dc90cffd1dbc6ccf6bde5df6d015b
lili <a1@gv-bhjnkmlfcszxdcfvghj231qwasZ21.local>
2024-07-02 01:53:10 +0800

    v1.1.9

--- test1/Test.go
+++ test1/Test.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"time"
-
-	"github.com/stormi-li/stormi"
+	"fmt"
 )
 
 func main() {
-	syp := stormi.NewSyncProxy(stormi.NewRedisProxy("127.0.0.1:213"))
-	cond := syp.NewCond("cond")
-	cond.Singal()
-	time.Sleep(2 * time.Second)
-	cond.Broadcast()
+	a:=10
+	fmt.Println(a)
 }
